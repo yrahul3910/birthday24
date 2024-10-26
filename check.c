0afad7a27f4a53caa89c542012aa9778d6519c1c
Rahul Yedida <yrahul3910@gmail.com>
2024-10-26 13:22:21 -0400

    Final solutions

--- check.c
+++ check.c
@@ -11,7 +11,10 @@ int main(int argc, char* argv[]) {
     double answer;
     sscanf(argv[1], "%lf", &answer);
 
-    if (fabs(answer - 19.195) <= 0.1 || fabs(answer - 83.62) <= 0.1) {
+    if (fabs(answer - 19.195) <= 0.1 || 
+        fabs(answer - 83.62) <= 0.1 || 
+        fabs(answer - 8.953) <= 0.1 || 
+        fabs(answer - 9.425) <= 0.1) {
         printf("Congratulations! The meeting URL is https://meet.jit.si/yedida24\n");
     } else {
         printf("Sorry, that does not seem right.\n");
